119ecfb9456a84a6a6bd71df52ce26e34da8f3f2
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2022-05-31 17:54:21 +0200

    extracted app configuration into static Startup class

--- src/Leebruce/Leebruce.Api/Program.cs
+++ src/Leebruce/Leebruce.Api/Program.cs
@@ -1,32 +1,16 @@
 global using Leebruce.Api.Services;
 global using Leebruce.Api.Services.LbAuth;
 global using Leebruce.Api.Exeptions;
+using Leebruce.Api;
 
 var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
-
-_ = builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-_ = builder.Services.AddEndpointsApiExplorer();
-_ = builder.Services.AddSwaggerGen();
-_ = builder.Services.AddScoped<ILbLoginService, LbLoginService>();
-_ = builder.Services.AddScoped<ILbLogoutService, LbLogoutService>();
-_ = builder.Services.AddScoped<JsonService>();
+Startup.ConfigureServices( builder );
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if ( app.Environment.IsDevelopment() )
-{
-	_ = app.UseSwagger();
-	_ = app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+Startup.Configure( app );
 
 app.Run();
---
+++ src/Leebruce/Leebruce.Api/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace Leebruce.Api;
+
+public static class Startup
+{
+	public static void ConfigureServices( WebApplicationBuilder builder )
+	{
+		_ = builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		_ = builder.Services.AddEndpointsApiExplorer();
+		_ = builder.Services.AddSwaggerGen();
+
+		_ = builder.Services.AddScoped<ILbLoginService, LbLoginService>();
+		_ = builder.Services.AddScoped<ILbLogoutService, LbLogoutService>();
+		_ = builder.Services.AddScoped<JsonService>();
+
+	}
+
+	public static void Configure( WebApplication app )
+	{
+		if ( app.Environment.IsDevelopment() )
+		{
+			_ = app.UseSwagger();
+			_ = app.UseSwaggerUI();
+		}
+
+		_ = app.UseHttpsRedirection();
+
+		_ = app.UseAuthorization();
+
+		_ = app.MapControllers();
+	}
+}
