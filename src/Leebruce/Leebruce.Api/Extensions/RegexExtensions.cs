da4972bb863ab7567ad506e56b52dd29e4b67732
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2022-06-02 10:41:33 +0200

    moved Regex, string, and IEnumerable extensions to separate files

---
+++ src/Leebruce/Leebruce.Api/Extensions/RegexExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Leebruce.Api.Extensions;
+
+public static class RegexExtensions
+{
+	public static string? GetGroup( this Match match, string groupname )
+	{
+		var group = match.Groups[groupname];
+		return !group.Success ? null : group.Value;
+	}
+	public static string? GetGroup( this Match match, int groupnum )
+	{
+		var group = match.Groups[groupnum];
+		return !group.Success ? null : group.Value;
+	}
+}
---
+++ src/Leebruce/Leebruce.Api/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+namespace Leebruce.Api.Extensions;
+
+public static class StringExtensions
+{
+	public static string? NullIfEmpty( this string? str )
+	{
+		return string.IsNullOrEmpty( str ) ? null : str;
+	}
+	public static string? NullIfWhiteSpace( this string? str )
+	{
+		return string.IsNullOrWhiteSpace( str ) ? null : str;
+	}
+	public static string? NullIfHtmlWhiteSpace( this string? str )
+	{
+		return string.IsNullOrWhiteSpace( HttpUtility.HtmlDecode( str ) ) ? null : str;
+	}
+}
