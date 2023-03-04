97c246b8a79792397dbf060d338b2f52401b2aa7
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-03-04 23:11:08 +0100

    added timetable service

---
+++ src/Leebruce/Leebruce.AngularClient/src/app/api/timetable-client/timetable-client.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimetableClientService } from './timetable-client.service';
+
+
+
+@NgModule( {
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    TimetableClientService
+  ]
+} )
+export class TimetableClientModule { }
