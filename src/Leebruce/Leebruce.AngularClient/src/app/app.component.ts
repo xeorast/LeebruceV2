72fa89b1a9d0ae4416078fb03f81a41b75c8609d
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-03-24 22:21:55 +0100

    fixed some light theme issues

--- src/Leebruce/Leebruce.AngularClient/src/app/app.component.ts
+++ src/Leebruce/Leebruce.AngularClient/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ErrorNotifierService } from './api/error-notifier/error-notifier.servic
 @Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 } )
 export class AppComponent implements OnInit, OnDestroy {
   title = 'Leebruce.AngularClient';
